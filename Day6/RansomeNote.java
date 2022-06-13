5d8b9211e7c06a703c2b0517187b739d12f25ff3
Artur Ardai <krkerdo@gmail.com>
2022-06-13 11:54:42 -1200

    new

---
+++ Day6/RansomeNote.java
@@ -0,0 +1,23 @@
+package Day6;
+
+public class RansomeNote {
+    public static void main(String[] args) {
+        
+    }
+    public boolean canConstruct(String ransomNote, String magazine) {
+       
+        char[]off=new char[128];
+        
+        
+        for (int i = 0; i < magazine.length(); i++) {
+            off[magazine.charAt(i)]++;
+        }
+        
+        for (int i = 0; i < ransomNote.length(); i++) {
+            if(off[ransomNote.charAt(i)]<=0)
+            return false;
+            off[ransomNote.charAt(i)]--;
+        }
+        return true;
+    }
+}
