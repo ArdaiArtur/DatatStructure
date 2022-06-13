c66126d1857e37b9de232fb32a16f7a33fc3d02d
Artur Ardai <krkerdo@gmail.com>
2022-06-12 15:22:41 -1200

    works

--- Day5/ValidSoduku.java
+++ Day5/ValidSoduku.java
@@ -9,18 +9,30 @@ public class ValidSoduku {
 
     public boolean isValidSudoku(char[][] board) {
      HashSet<Character>hs=new HashSet<Character>();
+     int x=0;
      for (int i = 0; i < board.length; i++) {
         for (int j = 0; j < board[0].length; j++) {
-            if(!hs.add(board[i][j]))
+            if(board[i][j]!='.'&&!hs.add(board[i][j]))
             return false;
         }
         hs.clear();
         for (int j = 0; j < board[0].length; j++) {
-            if(!hs.add(board[j][i]))
+            if(board[j][i]!='.'&&!hs.add(board[j][i]))
             return false;
         }
         hs.clear();
-        
-     }   
+        if(i!=0 && i%3==0   )
+        {
+            x+=3;
+        }
+        for (int j =x; j < x+3; j++) {
+            for (int j2 = i%3*3; j2 < i%3*3+3 ; j2++) {
+                if(board[j][j2]!='.'&&!hs.add(board[j][j2]))
+                return false;
+            }
+        }
+        hs.clear();
+     }
+     return true;   
     }
 }
